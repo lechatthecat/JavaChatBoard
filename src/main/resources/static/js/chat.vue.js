161cea0197905c9ec89e550b272d3113801f026a
Shu <shu845@gmail.com>
2019-01-17 21:36:33 +0900

    rewrite the js file with vue.js

--- src/main/resources/static/js/chat.vue.js
+++ src/main/resources/static/js/chat.vue.js
@@ -3,10 +3,11 @@ var stompClient = null;
 var isConnected = false;
 
 const messageFormButton = new Vue({
-    el: '#messageFormButton',
+    el: '.wrapper',
     data: {
-        messageToSend: [],
-        messageReceived: []
+        messageReceived: [],
+        joinMessageReceived: [],
+        username: username
     },
     methods: {
         send: function (event) {
@@ -65,37 +66,9 @@ const messageFormButton = new Vue({
         },
         onMessageReceived: function (payload) {
             var message = JSON.parse(payload.body);
-
             if (message.type === 'JOIN') {
                 if (!checkIfOnlineMarkAlreadyAdded("sender-" + message.sender)) {
-                    var contactElement = document.createElement('li');
-                    contactElement.id = "sender-" + message.sender;
-                    var messageImg = document.createElement('img');
-                    messageImg.classList.add("contacts-list-img");
-                    messageImg.setAttribute('src', message.userImagePath);
-                    contactElement.appendChild(messageImg);
-                    var contactNameElement = document.createElement('span');
-                    contactNameElement.classList.add("contacts-list-name");
-                    var senderNameText = document.createTextNode(message.sender + "  ");
-                    contactNameElement.appendChild(senderNameText);
-                    var onlineMark = document.createElement('i');
-                    onlineMark.id = "sender-currentstatus-" + message.sender;
-                    onlineMark.classList.add("fa");
-                    onlineMark.classList.add("fa-circle");
-                    onlineMark.classList.add("text-success");
-                    // var pullRight = document.createElement('small');
-                    // pullRight.classList.add("contacts-list-date");
-                    // pullRight.classList.add("pull-right");
-                    // pullRight.appendChild(onlineMark);
-                    // contactNameElement.appendChild(pullRight);
-                    contactNameElement.appendChild(onlineMark);
-                    var contactInfoElement = document.createElement('div');
-                    contactInfoElement.classList.add("contacts-list-info");
-                    var contactMsgElement = document.createElement('span');
-                    contactMsgElement.classList.add("contacts-list-msg");
-                    contactInfoElement.appendChild(contactNameElement);
-                    contactElement.appendChild(contactInfoElement);
-                    $("#contacts-list").append(contactElement);
+                    this.joinMessageReceived.push(message);
                 } else {
                     $("#sender-currentstatus-" + message.sender).removeClass("text-dark");
                     $("#sender-currentstatus-" + message.sender).addClass("text-success");
@@ -104,57 +77,7 @@ const messageFormButton = new Vue({
                 $("#sender-currentstatus-" + message.sender).removeClass("text-success");
                 $("#sender-currentstatus-" + message.sender).addClass("text-dark");
             } else {
-                var messageElement = document.createElement('div');
-                messageElement.classList.add("direct-chat-msg");
-                //Sender info
-                var messageSenderInfo = document.createElement('div');
-                messageSenderInfo.classList.add("direct-chat-info");
-                messageSenderInfo.classList.add("clearfix");
-
-                //Sender Name
-                var senderName = document.createElement('span');
-                senderName.classList.add("direct-chat-name");
-                if (message.sender == username) {
-                    messageElement.classList.add("right");
-                    senderName.classList.add("pull-right");
-                } else {
-                    messageElement.classList.add("left");
-                    senderName.classList.add("pull-left");
-                }
-                var senderNameText = document.createTextNode(message.sender);
-                senderName.appendChild(senderNameText);
-
-                //Time stamp
-                var timeStamp = document.createElement('span');
-                timeStamp.classList.add("direct-chat-timestamp");
-                if (message.sender == username) {
-                    timeStamp.classList.add("pull-left");
-                } else {
-                    timeStamp.classList.add("pull-right");
-                }
-                var timeStampText = document.createTextNode(message.updated);
-                timeStamp.appendChild(timeStampText);
-
-                //Sender image
-                var senderImage = document.createElement('img');
-                senderImage.classList.add("direct-chat-img");
-                senderImage.setAttribute('src', message.userImagePath);
-
-                //Message text
-                var chatMessage = document.createElement('pre');
-                chatMessage.classList.add("direct-chat-text");
-                var chatMessageText = document.createTextNode(message.response);
-                chatMessage.appendChild(chatMessageText);
-
-                messageSenderInfo.appendChild(senderName);
-                messageSenderInfo.appendChild(timeStamp);
-                messageElement.appendChild(messageSenderInfo);
-                messageElement.appendChild(senderImage);
-                messageElement.appendChild(chatMessage);
-
-                document.querySelector('#messageArea').appendChild(messageElement);
-                document.querySelector('#messageArea').scrollTop = document.querySelector('#messageArea').scrollHeight;
-
+                this.messageReceived.push(message);
             }
         }
     },

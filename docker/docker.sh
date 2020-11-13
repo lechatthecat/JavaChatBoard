f19f63b695353f3dbc68c4306194b2f0ddbc3704
lechatthecat <shu845@gmail.com>
2020-11-14 02:07:46 +0900

    change from mvn intall to mvn package

--- docker/docker.sh
+++ docker/docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /JavaChatBoard
-mvn clean install
+mvn clean package
 exec java -jar /JavaChatBoard/target/chatboard-0.0.1-SNAPSHOT.jar --spring.config.location=file:/JavaChatBoard/src/main/resources/docker.application.properties
 
 /bin/bash
